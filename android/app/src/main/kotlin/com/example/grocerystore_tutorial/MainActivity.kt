d767120493f68e3da2dcbcd3b450edf0b5d1cc53
fidelis <fidelisnwabisi@gmail.com>
2023-05-24 01:52:22 +0100

    First Commit

---
+++ android/app/src/main/kotlin/com/example/grocerystore_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.grocerystore_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // logo
+          Image.asset('lib/images/avocado.png'),
+
+          //we deliver groceries at your doorstep
+
+          //fresh items everyday
+
+          //fwr started button
+        ],
+      ),
+    );
+  }
+}
