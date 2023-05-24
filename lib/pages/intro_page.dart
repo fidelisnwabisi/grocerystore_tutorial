7401e7294abf6e6cb7f496bf465f6e51bb1a23f7
fidelis <fidelisnwabisi@gmail.com>
2023-05-24 02:12:40 +0100

    editing the intro_page.dart

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -9,13 +9,42 @@ class IntroPage extends StatelessWidget {
       body: Column(
         children: [
           // logo
-          Image.asset('lib/images/avocado.png'),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 88.0, right: 80, bottom: 40, top: 160),
+            child: Image.asset('lib/images/avocado.png'),
+          ),
 
           //we deliver groceries at your doorstep
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Text(
+              "We deliver groceries at your doorstep",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
 
           //fresh items everyday
+          const Text(
+            "Fresh items everyday",
+          ),
+          const Spacer(),
 
-          //fwr started button
+          //get started button
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(12)),
+            padding: const EdgeInsets.all(24),
+            child: const Text(
+              "Get Started",
+              style: TextStyle(color: Colors.white),
+            ),
+          )
         ],
       ),
     );
